e7cd79b5b2bf614ced0634aaf8722639d48ffe1b
Camilo Ramirez <camilor@gmail.com>
2019-01-05 19:42:09 -0500

    distances done, but from categorical applied to ordinal

--- segment.R
+++ segment.R
@@ -12,9 +12,9 @@ whisky_df <- read_csv("data/raw/whisky.csv")
 completre <-whisky_df[complete.cases(whisky_df),]
 
 ld_dmy <- caret::dummyVars(" ~ . ", data =completre, fullRank = TRUE)
-trasf_ld <- data.frame(predict(ld_dmy, newdata =completre))
+trasf_ld <- data.frame(predict(ld_dmy, newdata = completre))
 distance_trasf_ld <- cluster::daisy (trasf_ld,
-                           type = list ( asymm = c( 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 40, 41, 42, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76)),
+#                           type = list ( asymm = c( 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 40, 41, 42, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76)),
                             metric= "gower")
 distance_trasf_ld.mat <- as.matrix(distance_trasf_ld)
 
