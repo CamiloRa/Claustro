3d958aa07b64ef5e32892ea6b2210dad6b0c5e68
Camilo Ramirez <camilor@gmail.com>
2019-01-07 10:07:01 -0500

    Start with the function

--- cleaning.R
+++ cleaning.R
@@ -1,6 +1,24 @@
-#reading data in a DF, later to turn into list.
+#reading data in a DF, later to turn into categories
+#load libraries
+library(dplyr)
 
-plants_df <- readLines("data/raw/plants.data.csv" )
-head(plants_df)
+#load functions
+readfirstcolumn <- function  (dataframe) {
+  return(nrow (dataframe))
+}
 
-str(plants_df[4])
+#read data
+plants <- readLines("data/raw/plants.data.csv" )
+plants_df <- as.data.frame(plants)
+
+#make a column
+readfirstcolumn(plants_df)
+
+  
+
+
+str(plants_df[4,]
+plants_df[4:8,]
+colnames(plants_df)
+colnames(plants_df) <- "plant_name"
+dim(plants_df)
